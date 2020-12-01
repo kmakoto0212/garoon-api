e7e7be4beaa0fabc3e22fe47a148b16d4976c853
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-01 17:46:05 +0900

    getUsersUrl関数は複数ページに跨る場合でも再帰的にURLを取得するように

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,21 +1,34 @@
 import { createBrowser } from "./lib/Browser";
 import { login } from "./garoon";
 import { auth } from "./types/auth";
+import { URL } from "url";
+import { Browser } from "puppeteer";
 
 export const getUsersUrl = async (option: {
   url: string;
   auth: auth;
+  offset?: number;
+  browser?: Browser;
 }): Promise<string[]> => {
-  const browser = await createBrowser();
-  const page = await browser.newPage();
-  await page.goto(option.url, {
-    waitUntil: "networkidle2",
+  option.offset ??= 0;
+  const browser = option.browser || (await createBrowser());
+  const [page] = await browser.pages();
+  const _url = new URL(option.url);
+  _url.searchParams.append("sp", option.offset.toString());
+
+  await page.goto(_url.href, {
+    waitUntil: "domcontentloaded",
   });
-  await login(page, option.auth);
-  const hrefs = await page.$$eval(
-    ".profileImageBaseSchedule-grn > dl > dd > a",
-    (elems) => elems.map((elem) => elem.getAttribute("href"))
+  page.waitForTimeout(1000);
+  if (!option.browser) await login(page, option.auth);
+  let hrefs = await page.$$eval("dd > a", (elems) =>
+    elems.map((elem) => elem.getAttribute("href"))
   );
+  if (hrefs.length >= 29) {
+    option.browser ??= browser;
+    option.offset = option.offset + 30;
+    hrefs = hrefs.concat(await getUsersUrl(option));
+  }
   browser.close();
 
   return hrefs;
