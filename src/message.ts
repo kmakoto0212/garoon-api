5683d9e60ad693dd8682d1715b349a4b8bdc7979
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-01 17:44:58 +0900

    宛先を2件以上追加出来なかった問題を修正

--- src/message.ts
+++ src/message.ts
@@ -2,6 +2,21 @@ import { createBrowser } from "./lib/Browser";
 import { login } from "./garoon";
 import { auth } from "./types/auth";
 
+const selector = {
+  inputTitle:
+    "#body > div.mainarea > form > table > tbody > tr:nth-child(1) > td > input[type=text]",
+  inputBody: "#data_editor_id",
+  inputUser: "#keyword_user",
+  selectUser: "#ul_selectlist_CID > li",
+  addUserButton: "#btn_add_sUID",
+  acknowledgment: "#\\33 _label",
+  maintainer:
+    "#body > div.mainarea > form > table > tbody > tr:nth-child(7) > td > label:nth-child(4)",
+  submitMaintainerAll: "#btn_add_sUID_o",
+  sendSubmit: "#message_button_send > a",
+  draftSubmit: "#message_button_draft > a",
+};
+
 export const postMailMessage = async (option: {
   url: string;
   auth: auth;
@@ -11,26 +26,13 @@ export const postMailMessage = async (option: {
   acknowledgment?: boolean;
   MaintainerAll?: boolean;
   isDraft?: boolean;
+  lazy?: number;
 }): Promise<void> => {
   const browser = await createBrowser({
     headless: false,
     defaultViewport: { width: 0, height: 0 },
   });
-  const page = await browser.newPage();
-  const selector = {
-    inputTitle:
-      "#body > div.mainarea > form > table > tbody > tr:nth-child(1) > td > input[type=text]",
-    inputBody: "#data_editor_id",
-    inputUser: "#keyword_user",
-    selectUser: ".selectlist_text_grn",
-    addUserButton: "#btn_add_sUID",
-    acknowledgment: "#\\33 _label",
-    maintainer:
-      "#body > div.mainarea > form > table > tbody > tr:nth-child(7) > td > label:nth-child(4)",
-    submitMaintainerAll: "#btn_add_sUID_o",
-    sendSubmit: "#message_button_send > a",
-    draftSubmit: "#message_button_draft > a",
-  };
+  const [page] = await browser.pages();
   await page.goto(option.url, {
     waitUntil: "networkidle2",
   });
@@ -45,9 +47,14 @@ export const postMailMessage = async (option: {
     const inputUser = await page.$(selector.inputUser);
     await inputUser.type(option.to[i]);
     await inputUser.press("Enter");
-    await page.waitForSelector(selector.selectUser);
-    await page.$(selector.selectUser).then((e) => e.focus());
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
+    await page.waitForTimeout(option.lazy || 1000);
+    await page.$$(selector.selectUser).then((e) => {
+      if (e.length) {
+        e[0].click();
+      } else {
+        throw new Error(`${option.to[i]} is notfound...`);
+      }
+    });
     await page.click(selector.addUserButton);
   }
 
@@ -58,8 +65,9 @@ export const postMailMessage = async (option: {
     await page.click(selector.maintainer);
     await page.click(selector.submitMaintainerAll);
   }
+
+  await page.waitForTimeout(1000);
   if (option.isDraft) {
-    await page.waitForNavigation({ waitUntil: "networkidle0" });
     await page.click(selector.draftSubmit);
   } else {
     await page.click(selector.sendSubmit);
