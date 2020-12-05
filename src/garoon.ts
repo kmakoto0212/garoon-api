a6498e287f21171b6cd72eb800998bcc3bed2688
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-05 23:32:33 +0900

    login関数は既にログインしているときも正常終了するように

--- src/garoon.ts
+++ src/garoon.ts
@@ -1,10 +1,23 @@
 import { Page } from "puppeteer";
 import { auth } from "./types/auth";
 
+const loginSelector = {
+  dialog: "#login-dialog",
+  inputUserName: "#username-\\:0-text",
+  inputPassword: "#password-\\:1-text",
+  submit: ".login-button",
+};
+
+export const isLogin = async (page: Page): Promise<boolean> => {
+  return !!(await page.$(loginSelector.dialog));
+};
+
 export const login = async (page: Page, auth: auth): Promise<void> => {
-  await page.type("#username-\\:0-text", auth.username);
-  await page.type("#password-\\:1-text", auth.password);
-  await page.click(".login-button");
+  if (!(await isLogin(page))) return;
+
+  await page.type(loginSelector.inputUserName, auth.username);
+  await page.type(loginSelector.inputPassword, auth.password);
+  await page.click(loginSelector.submit);
 
   await page.waitForNavigation({
     waitUntil: "domcontentloaded",
