8f5817b960b0b40741bfea517fea60f873cfad1a
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-07 15:29:21 +0900

    Promise.all()の同時実行数を制限するヘルパー関数を追加

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,18 @@
+import pLimit from "p-limit";
+
 export const isValidateUrl = (url: string): boolean => {
   return /https:\/\/[a-zA-Z0-9]+.cybozu.com\//.test(url);
+};
+
+export const promiseAll = async <T>(
+  promises: Promise<T>[],
+  limit: number
+): Promise<T[]> => {
+  const Limit = pLimit(limit);
+  const _promises = promises.map((p) => {
+    return Limit(() => p);
+  });
+
+  const result = await Promise.all(_promises);
+  return result;
 };
