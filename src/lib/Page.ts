7ec500095eb50181bb22b9fc48df163a8dc0ac06
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-07 15:28:06 +0900

    単純処理の最適化を行うページ作成のヘルパー関数を追加

--- src/lib/Page.ts
+++ src/lib/Page.ts
@@ -1,4 +1,40 @@
-import { Page } from "puppeteer";
+import { Browser, Page } from "puppeteer";
+import { login } from "../garoon";
+import { auth } from "../types/auth";
+
+export const createPage = async (
+  browser: Browser,
+  option?: {
+    noImages?: boolean;
+    preLogin?: {
+      url: string;
+      auth: auth;
+    };
+  }
+): Promise<Page> => {
+  const page = await browser.newPage();
+  option ??= {};
+  option.noImages ??= true;
+  if (option.noImages) {
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (
+        ["image", "stylesheet", "font"].indexOf(request.resourceType()) !== -1
+      ) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+  }
+  if (option.preLogin) {
+    await page.goto(option.preLogin.url, {
+      waitUntil: "networkidle0",
+    });
+    await login(page, option.preLogin.auth);
+  }
+  return page;
+};
 
 export const getNodeToString = async (
   page: Page,
