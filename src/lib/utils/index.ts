23b5827b2df1aa485a612adb65e352676ae5c585
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2020-12-15 17:52:33 +0900

    getISOString関数はガルーンの日付形式をISO形式の日付文字列に変換するように

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,7 +1,7 @@
 import pLimit from "p-limit";
 
 export const isValidateUrl = (url: string): boolean => {
-  return /https:\/\/[a-zA-Z0-9]+.cybozu.com\//.test(url);
+  return /https:\/\/[a-zA-Z0-9]+\.cybozu\.com\//.test(url);
 };
 
 export const promiseAll = async <T>(
@@ -15,4 +15,20 @@ export const promiseAll = async <T>(
 
   const result = await Promise.all(_promises);
   return result;
+};
+
+export const getISOString = (date: string): string => {
+  const [
+    ,
+    year,
+    month,
+    day,
+    hour,
+    minitts,
+  ] = /([0-9]{4})年([0-9]{2})月([0-9]{2})日（.{1}） ([0-9]{2}):([0-9]{2})/.exec(
+    date
+  );
+  const strDate = `${year}-${month}-${day}T${hour}:${minitts}:00`;
+  const Data = new Date(strDate);
+  return Data.toISOString();
 };
