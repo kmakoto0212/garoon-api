5dc64bbd32df3286b4f4576d45832a5a14f8345c
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2021-02-12 17:29:38 +0900

    ビルドファイルの更新

--- dist/mailBox/index.js
+++ dist/mailBox/index.js
@@ -2,9 +2,10 @@ import { createBrowser } from "../lib/browser";
 import { login } from "..";
 import { URL } from "url";
 import { getFullUrl } from "../lib/utils";
+import { createPage } from "../lib/page";
 const mailBoxPageSelector = {
   mailList: "td > span > a",
-  mailBox: "#message_list > tbody > tr",
+  mail: "#message_list > tbody > tr",
   moveTo: "#dcid1",
   allMailsCheckButton:
     "#view_part > div:nth-child(3) > span:nth-child(1) > button",
@@ -14,7 +15,7 @@ export const getMails = async (option) => {
   var _a, _b;
   (_a = option.offset) !== null && _a !== void 0 ? _a : (option.offset = 0);
   const browser = option.browser || (await createBrowser());
-  const [page] = await browser.pages();
+  const page = await createPage(browser);
   const _url = new URL(option.url);
   _url.searchParams.append("sp", option.offset.toString());
   await page.goto(_url.href, {
@@ -51,22 +52,30 @@ export const getMails = async (option) => {
     option.offset = option.offset + 30;
     mails = mails.concat(await getMails(option));
   }
-  browser.close();
+  if (option.browser) {
+    await page.close();
+  } else {
+    await browser.close();
+  }
   return mails;
 };
 export const moveMails = async (option) => {
   const browser = option.browser || (await createBrowser({}));
-  const [page] = await browser.pages();
+  const page = await createPage(browser);
   await page.goto(option.url, {
     waitUntil: "networkidle0",
   });
-  if (!option.browser) await login(page, option.auth);
-  while ((await page.$$(mailBoxPageSelector.mailBox)).length > 1) {
+  await login(page, option.auth);
+  while ((await page.$$(mailBoxPageSelector.mail)).length > 1) {
     await page.click(mailBoxPageSelector.allMailsCheckButton);
     await page.select(mailBoxPageSelector.moveTo, option.moveToCid);
     await page.click(mailBoxPageSelector.moveSubmit);
     await page.waitForTimeout(option.delay || 1000);
   }
-  await browser.close();
+  if (option.browser) {
+    await page.close();
+  } else {
+    await browser.close();
+  }
   return;
 };
